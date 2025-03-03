98532743b81df2442aaac1581dfb48f9ccc2ba09
Shohruz Ernazarov <shohruzernazarov04@gmail.com>
2025-03-02 20:30:52 -0500

    Remove unnecessary code

--- web-app/src/app/page.tsx
+++ web-app/src/app/page.tsx
@@ -1,5 +1,3 @@
-import { QueryClient, Hydrate, dehydrate } from "@tanstack/react-query";
-import { getCryptoData } from "@/lib/api";
 import { Suspense } from "react";
 import SearchBar from "@/components/search-bar";
 import CryptoTracker from "@/components/crypto-tracker";
@@ -7,8 +5,6 @@ import LoadingSkeleton from "@/components/loading-skeleton";
 import DataProvider from "@/components/DataProvider";
 
 export default async function Home() {
-  const queryClient = new QueryClient();
-
   return (
     <div className="min-h-screen bg-gradient-to-b from-slate-900 to-slate-800 text-white">
       <div className="container mx-auto px-2 py-8">
@@ -17,11 +13,9 @@ export default async function Home() {
         <p className="text-slate-500 mb-8">By Shohruz Ernazarov</p>
         <SearchBar />
         <DataProvider>
-          <Hydrate state={dehydrate(queryClient)}>
-            <Suspense fallback={<LoadingSkeleton />}>
-              <CryptoTracker />
-            </Suspense>
-          </Hydrate>
+          <Suspense fallback={<LoadingSkeleton />}>
+            <CryptoTracker />
+          </Suspense>
         </DataProvider>
       </div>
     </div>
